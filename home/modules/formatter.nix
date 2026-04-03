ad5bc550f61d4e27a0ac725f5b860e0d2ebcb928
M. Irwan Asikin <225542013+mirwanasikin@users.noreply.github.com>
2026-04-03 17:44:12 +0700

    feat: Add new formatter
    - hadolint
    - ansible-lint
    - tidy the formatter.nix

--- home/modules/formatter.nix
+++ home/modules/formatter.nix
@@ -3,15 +3,32 @@
 {
   # Formatter I Need
   home.packages = with pkgs; [
+    # Shell
     shfmt
+    shellcheck
+
+    # Nix
     nil
     nixfmt
+
+    # Python
     pyright
     ruff
+
+    # yaml / config
     yaml-language-server
+    prettier
+
+    # Docker
     dockerfile-language-server
     docker-compose-language-service
+    hadolint
+
+    # Ansible
+    ansible-lint
+
+    # terrafrom
     terraform-ls
-    prettier
+    tflint
   ];
 }
